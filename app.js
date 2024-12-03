3e33b8ed8bb19f3397f8fe64b043a0bfaf86799d
Jinu <jinujohn081@gmail.com>
2024-12-03 21:24:31 +0100

    modified add and package json file

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import postRoute from './routes/post.route.js'
-import authRoute from './routes/auth.route.js'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import postRoute from './routes/post.route.js';
+import authRoute from './routes/auth.route.js';
+const PORT = process.env.PORT || 8800;
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api/post', postRoute)
+app.use('/api/post', postRoute);
 
-app.use('/api/auth', authRoute)
+app.use('/api/auth', authRoute);
 
-app.listen(8800, () => {
-  console.log('Server is runing')
-})
+app.listen(PORT, () => {
+  console.log('Server is runing');
+});
